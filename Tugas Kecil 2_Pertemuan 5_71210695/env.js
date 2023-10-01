b32901561efc09751eb197fafff75472b109fb65
cahaya-sampebua <93310826+cahaya-sampebua@users.noreply.github.com>
2023-10-01 22:33:43 +0700

    Add files via upload

---
+++ Tugas Kecil 2_Pertemuan 5_71210695/env.js
@@ -0,0 +1,12 @@
+// env.js
+const cat_env = {
+  API_KEY: "live_tOqt81j977NpFfd8xyofyUzbefy3Qurr948vvE7zqbm1MZGgp3oaG5xyS16kgi86",
+  endpoint: "https://api.thedogapi.com/",
+};
+
+const dog_env = {
+  API_KEY: "live_tOqt81j977NpFfd8xyofyUzbefy3Qurr948vvE7zqbm1MZGgp3oaG5xyS16kgi86",
+  endpoint: "https://api.thedogapi.com/",
+};
+
+export { cat_env,dog_env};
